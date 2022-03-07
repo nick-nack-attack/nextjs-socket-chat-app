822c66389b51703513ff21f2f0e693cc3a7e7588
Nick Nack Attack <nickfarni001@gmail.com>
2022-03-06 17:24:15 -0700

    list of messages to be used as substitute messages

---
+++ utils/approvedMessages.tsx
@@ -0,0 +1,20 @@
+const approvedMessages = [
+  "Yes, we are the best nation in the world",
+  "Of course, I agree with everything state-sponsored television tells me",
+  "I don't believe anything that our great leader doesn't say",
+  "We must make the young into a generation of Communists.",
+  "Children, like soft wax, are very malleable and they should be moulded into good Communists.",
+  "We must rescue children from the harmful influence of the family.",
+  "Class hatred should be cultivated by an organic revulsion as far as the enemy is concerned.",
+  "Enemies must be seen as inferior.",
+  "I believe quite profoundly that the enemy is our inferior, and is a degenerate not only in the physical plane but also in the moral sense.",
+  "Shoot these rabid dogs. Death to this gang who hide their ferocious teeth, their eagle claws, from the people!",
+  "Down with these abject animals!",
+  "Let's exterminate the mad dogs of capitalism, who want to tear to pieces the flower of our new nation!",
+  "Long live powerful aviation of the socialist country!",
+  "Compulsory military education strengthens our defense!",
+  "For the Motherland, Honor and Freedom!",
+  "Let's crush the enemy under avalanche of steel",
+];
+
+module.exports = approvedMessages;
